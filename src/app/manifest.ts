3c9e3a72ff807d554a7ca80af8ab7c46b0f69065
oozmakafa <reymbrant.jbsa@gmail.com>
2025-12-28 01:01:54 +0800

    new logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,12 +9,55 @@ export default function manifest(): MetadataRoute.Manifest {
         display: "standalone",
         background_color: "#5528D8",
         theme_color: "#5528D8",
-        icons: [
+        "icons": [
             {
-                src: "/logo.png",
-                sizes: "1024x1024",
-                type: "image/png",
+                "src": "/logo-72.png",
+                "sizes": "72x72",
+                "type": "image/png",
+                "purpose": "any"
             },
-        ],
+            {
+                "src": "/logo-96.png",
+                "sizes": "96x96",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-128.png",
+                "sizes": "128x128",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-144.png",
+                "sizes": "144x144",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-152.png",
+                "sizes": "152x152",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-384.png",
+                "sizes": "384x384",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-512.png",
+                "sizes": "512x512",
+                "type": "image/png",
+                "purpose": "any"
+            }
+        ]
     };
 }
