64f884181b530be7064a18ce3853ed065b769df0
oozmakafa <reymbrant.jbsa@gmail.com>
2025-12-28 01:05:00 +0800

    Feature/new logo (#41)
    New Logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,23 +3,61 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
     return {
         name: "Stremio Account Addon Cloner",
-        short_name: "Addon Cloner",
+        short_name: "Stremio Addon Cloner",
         description: "Stremio Account Addon Cloner PWA",
         start_url: "/",
         display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        icons: [
+        background_color: "#5528D8",
+        theme_color: "#5528D8",
+        "icons": [
             {
-                src: "/logo.svg",
-                sizes: "192x192",
-                type: "image/png",
+                "src": "/logo-72.png",
+                "sizes": "72x72",
+                "type": "image/png",
+                "purpose": "any"
             },
             {
-                src: "/logo.svg",
-                sizes: "500x500",
-                type: "image/png",
+                "src": "/logo-96.png",
+                "sizes": "96x96",
+                "type": "image/png",
+                "purpose": "any"
             },
-        ],
+            {
+                "src": "/logo-128.png",
+                "sizes": "128x128",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-144.png",
+                "sizes": "144x144",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-152.png",
+                "sizes": "152x152",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-384.png",
+                "sizes": "384x384",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "/logo-512.png",
+                "sizes": "512x512",
+                "type": "image/png",
+                "purpose": "any"
+            }
+        ]
     };
 }
